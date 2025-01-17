5ae6a08aa311e6b207e5c86693bb00aed917dce8
Joyless <65855333+Joy-less@users.noreply.github.com>
2025-01-17 13:13:53 +0000

    Improve benchmarks

--- HjsonSharp.Benchmarks/Program.cs
+++ HjsonSharp.Benchmarks/Program.cs
@@ -13,39 +13,52 @@ public class Program {
     public static void ProfilePerformance() {
         HjsonSharpVsHjsonCsBenchmarks Benchmarks = new();
         while (true) {
-            Benchmarks.ParseLongStringHjsonSharp();
-            Benchmarks.ParseShortIntegerRepeatedHjsonSharp();
-            Benchmarks.ParsePersonHjsonSharp();
+            Benchmarks.LongStringHjsonSharp();
+            Benchmarks.ShortIntegerHjsonSharp();
+            Benchmarks.PersonHjsonSharp();
         }
     }
 }
 
 [MemoryDiagnoser]
 public class HjsonSharpVsHjsonCsBenchmarks {
+    #region Long String
+
+    private const int LongStringIterations = 1;
+
     private static readonly string LongString = new('a', 1_000_000);
     private static readonly string LongStringJson = '"' + LongString + '"';
 
-    [Benchmark]
+    [Benchmark(OperationsPerInvoke = LongStringIterations)]
     public void LongStringHjsonCs() {
-        string Result = (string)HjsonValue.Parse(LongStringJson);
-        if (Result != LongString) {
-            throw new Exception();
+        for (int Counter = 0; Counter < LongStringIterations; Counter++) {
+            string Result = (string)HjsonValue.Parse(LongStringJson);
+            if (Result != LongString) {
+                throw new Exception();
+            }
         }
     }
-    [Benchmark]
-    public void ParseLongStringHjsonSharp() {
-        string Result = HjsonReader.ParseElement<string>(LongStringJson)!;
-        if (Result != LongString) {
-            throw new Exception();
+    [Benchmark(OperationsPerInvoke = LongStringIterations)]
+    public void LongStringHjsonSharp() {
+        for (int Counter = 0; Counter < LongStringIterations; Counter++) {
+            string Result = HjsonReader.ParseElement<string>(LongStringJson)!;
+            if (Result != LongString) {
+                throw new Exception();
+            }
         }
     }
 
-    private static readonly int ShortInteger = 123;
+    #endregion
+
+    #region Short Integer
+
+    private const int ShortIntegerIterations = 1_000_000;
+
+    private static readonly int ShortInteger = 12345;
     private static readonly string ShortIntegerJson = ShortInteger.ToString();
-    private static readonly int ShortIntegerIterations = 1_000_000;
 
-    [Benchmark]
-    public void ParseShortIntegerRepeatedHjsonCs() {
+    [Benchmark(OperationsPerInvoke = ShortIntegerIterations)]
+    public void ShortIntegerHjsonCs() {
         for (int Counter = 0; Counter < ShortIntegerIterations; Counter++) {
             int Result = (int)HjsonValue.Parse(ShortIntegerJson);
             if (Result != ShortInteger) {
@@ -53,8 +66,8 @@ public class HjsonSharpVsHjsonCsBenchmarks {
             }
         }
     }
-    [Benchmark]
-    public void ParseShortIntegerRepeatedHjsonSharp() {
+    [Benchmark(OperationsPerInvoke = ShortIntegerIterations)]
+    public void ShortIntegerHjsonSharp() {
         for (int Counter = 0; Counter < ShortIntegerIterations; Counter++) {
             int Result = HjsonReader.ParseElement<int>(ShortIntegerJson)!;
             if (Result != ShortInteger) {
@@ -63,31 +76,47 @@ public class HjsonSharpVsHjsonCsBenchmarks {
         }
     }
 
+    #endregion
+
+    #region Person
+
+    private const int PersonIterations = 100_000;
+
     private static readonly Person Person = new() {
         Name = "John Doe",
         Age = 30,
         Gender = "M",
+        Email = "johndoe1234567890!!!@example.com",
+        HeightMeters = 1.8,
     };
     private static readonly string PersonJson = JsonSerializer.Serialize(Person);
 
-    [Benchmark]
-    public void ParsePersonHjsonCs() {
-        Person Result = JsonSerializer.Deserialize<Person>(HjsonValue.Parse(PersonJson).ToString(Stringify.Plain))!;
-        if (Result != Person) {
-            throw new Exception();
+    [Benchmark(OperationsPerInvoke = PersonIterations)]
+    public void PersonHjsonCs() {
+        for (int Counter = 0; Counter < PersonIterations; Counter++) {
+            Person Result = JsonSerializer.Deserialize<Person>(HjsonValue.Parse(PersonJson).ToString(Stringify.Plain))!;
+            if (Result != Person) {
+                throw new Exception();
+            }
         }
     }
-    [Benchmark]
-    public void ParsePersonHjsonSharp() {
-        Person Result = HjsonReader.ParseElement<Person>(PersonJson)!;
-        if (Result != Person) {
-            throw new Exception();
+    [Benchmark(OperationsPerInvoke = PersonIterations)]
+    public void PersonHjsonSharp() {
+        for (int Counter = 0; Counter < PersonIterations; Counter++) {
+            Person Result = HjsonReader.ParseElement<Person>(PersonJson)!;
+            if (Result != Person) {
+                throw new Exception();
+            }
         }
     }
+
+    #endregion
 }
 
 public record Person {
     public required string Name { get; set; }
     public required int Age { get; set; }
     public required string Gender { get; set; }
+    public required string Email { get; set; }
+    public required double HeightMeters { get; set; }
 }
