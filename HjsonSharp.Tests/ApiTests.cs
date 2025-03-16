01d149686fda67430c9fdcafa530af15580aace6
Joyless <65855333+Joy-less@users.noreply.github.com>
2025-03-16 22:44:33 +0000

    Add usings to `ApiTests`

--- HjsonSharp.Tests/ApiTests.cs
+++ HjsonSharp.Tests/ApiTests.cs
@@ -5,23 +5,23 @@ namespace HjsonSharp.Tests;
 public class ApiTests {
     [Fact]
     public void ReadToEndStringTest() {
-        CustomJsonReader HjsonReader = new("abcdef");
+        using CustomJsonReader HjsonReader = new("abcdef");
         HjsonReader.ReadToEnd().ShouldBe("abcdef");
     }
     [Fact]
     public void ReadToEndBytesTest() {
-        CustomJsonReader HjsonReader = new(Encoding.UTF8.GetBytes("abcdef"));
+        using CustomJsonReader HjsonReader = new(Encoding.UTF8.GetBytes("abcdef"));
         HjsonReader.ReadToEnd().ShouldBe("abcdef");
     }
     [Fact]
     public void ReadToEndStreamTest() {
         using MemoryStream MemoryStream = new(Encoding.UTF8.GetBytes("abcdef"));
-        CustomJsonReader HjsonReader = new(MemoryStream);
+        using CustomJsonReader HjsonReader = new(MemoryStream);
         HjsonReader.ReadToEnd().ShouldBe("abcdef");
     }
     [Fact]
     public void ReadToEndListTest() {
-        CustomJsonReader HjsonReader = new([new Rune('a'), new Rune('b'), new Rune('c'), new Rune('d'), new Rune('e'), new Rune('f')]);
+        using CustomJsonReader HjsonReader = new([new Rune('a'), new Rune('b'), new Rune('c'), new Rune('d'), new Rune('e'), new Rune('f')]);
         HjsonReader.ReadToEnd().ShouldBe("abcdef");
     }
 }
